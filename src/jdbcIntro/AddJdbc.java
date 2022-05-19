f5c04b1a5b84a34d79eff667139886067d4810b7
Chaitanya Kumar <ChaitanyaUpadhyay95@gmail.com>
2022-05-19 21:50:04 +0530

    First Commit. Includes All files

---
+++ src/jdbcIntro/AddJdbc.java
@@ -0,0 +1,37 @@
+package jdbcIntro;
+
+// importing package
+
+import java.sql.*;
+
+public class AddJdbc {
+
+	public static void main(String[] args) throws Exception
+	{
+		String url="jdbc:mysql://localhost:3306/student";
+		String name="root";
+		String pass="8789";
+		String query="Select * from user";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection(url,name,pass);
+		Statement st=con.createStatement();
+		
+		// execute query is used to fetch the information from database
+		ResultSet rs=st.executeQuery(query);
+		
+		
+		
+		String userData ="";
+		
+		while(rs.next())
+		{
+			userData=rs.getInt(1)+ "  :  "+ rs.getString(2)+ "  :  "+ rs.getString(3);
+		    System.out.println(userData);
+		}    
+		
+		
+		
+	}
+
+}
